3928665d58a5652eebce0bcf74fc9b0d6ae192e0
pedropaulobt <pedropaulobrasca@gmail.com>
2023-05-04 18:43:56 -0300

    Adicionar arquivos de projeto.

---
+++ framework.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Excluir itens raramente utilizados dos cabeçalhos do Windows
+// Arquivos de Cabeçalho do Windows
+#include <windows.h>
+#include <iostream>
+#include "hook.h"
+#include "mem.h"
